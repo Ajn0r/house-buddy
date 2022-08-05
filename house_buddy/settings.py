2875165dfae50d8dd6f5f13f9ffec53f7db22aa8
Ajn0r <ronjasjogren@hotmail.com>
2022-08-05 09:35:17 +0000

    second try deployment commit

--- house_buddy/settings.py
+++ house_buddy/settings.py
@@ -25,7 +25,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-jck#w9iz_!@$b^%prr88j$6(*s(2sdoo#)4ae=l2uiaisz)3q6'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
