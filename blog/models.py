f98b632145c7ed6e56a6bd2741ded8582ee65344
Ajn0r <ronjasjogren@hotmail.com>
2022-08-05 10:35:45 +0000

    add models for blog app

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,50 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+# Models for the blog
+BLOG_CATEGORIES = (
+    (0, 'Unspecified'),
+    (1, 'Homeimprovments'),
+    (2, 'Savings'),
+    (3, 'Garden'),
+    (4, 'Usefull tips'),
+    (5, 'Home'),
+    (6, 'Evereyday life'),
+    (7, 'Other')
+)
+
+class Blogpost(models.Model):
+    title = models.CharField(max_length=80, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    image = CloudinaryField("image", default='placeholder')
+    content = models.TextField()
+    created_on = models.DateField(auto_now_add=True)
+    updated_on = models.DateField(auto_now=True)
+    category = models.IntegerField(choices=BLOG_CATEGORIES, default=0)
+    likes = models.ManyToManyField(User, related_name='blog_likes', blank=True)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.title
+
+    def total_likes(self):
+        return self.likes.count()
+
+
+# model for comments
+class Comments(models.Model):
+    post = models.ForeignKey(Blogpost, on_delete=models.CASCADE)
+    name = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField(max_length=300)
+    posted_on = models.DateTimeField(auto_now_add=True)
+    apporoved = models.BooleanField(default=False)
+    
+    class Meta:
+        ordering = ['posted_on']
+
+    def __str__(self):
+        return f'Comment {self.content} by {self.name}'
+
