13f8e8d7b8c5475828f8f833da35be7d2a2155b7
Ajn0r <ronjasjogren@hotmail.com>
2022-08-05 13:42:41 +0000

    add admin function to receipt organizer for testing

--- receipts_organizer/admin.py
+++ receipts_organizer/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
+from .models import Categories, Entries
+from django_summernote.admin import SummernoteModelAdmin
 # Register your models here.
+
+
+@admin.register(Categories)
+class CategoriesAdmin(SummernoteModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+    list_display = ['name']
+
+
+@admin.register(Entries)
+class EntriesAdmin(SummernoteModelAdmin):
+    list_display = ('title', 'category', 'amount')
