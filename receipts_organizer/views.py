e82ebd35fd18bf6923117fd2403b866a575b9832
Ajn0r <ronjasjogren@hotmail.com>
2022-08-05 14:52:10 +0000

    add view and url for categories model

--- receipts_organizer/views.py
+++ receipts_organizer/views.py
@@ -1,1 +1,10 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Categories, Entries
+
+
+class CategoryList(generic.ListView):
+    model = Categories
+    queryset = Categories.objects.order_by('name')
+    template_name = 'category_page.html'
+    paginate_by = 6
