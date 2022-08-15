f9511a58a7cdac35ea658a4209808177b7d8eeac
Ajn0r <ronjasjogren@hotmail.com>
2022-08-15 09:09:26 +0000

    add basic setup for viewing details of blogpost

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 
 urlpatterns = [
-    path('blog', views.BlogList.as_view(), name='blog')
+    path('blog', views.BlogList.as_view(), name='blog'),
+    path('<slug:slug>', views.BlogDetail.as_view(), name='blogpost'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Blogpost, Comments
 
 
@@ -7,3 +7,14 @@ class BlogList(ListView):
     model = Blogpost
     template_name = 'blog.html'
 
+
+class BlogDetail(DetailView):
+    model = Blogpost
+    template_name = 'blogpost_detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        post_detail = self.object
+        context['object'] = post_detail
+        context['blogpost'] = Blogpost.objects.filter(title=post_detail)
+        return context
