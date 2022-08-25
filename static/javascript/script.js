c2cc3970ba65dd49121b6a285240913980138b51
Ajn0r <ronjasjogren@hotmail.com>
2022-08-25 07:22:04 +0000

    move contact javascript to contact.html to resolve error

--- static/javascript/script.js
+++ static/javascript/script.js
@@ -18,37 +18,3 @@ newsletter.addEventListener('submit', function (e) {
     e.preventDefault();
     newsletter.innerHTML = '<p>Thank you for subscribing!</p>'
 })
-
-
-/**
- * Code to send email to house buddy mail
- * Code for setting up Emailjs comes from 
- * the documentation or their webpage
- * https://www.emailjs.com/docs/sdk/send/
- * The code for the modal is from: 
- * https://www.w3schools.com/howto/howto_css_modals.asp
- */
-window.onload = function () {
-    let form = document.getElementById('contact-form')
-    let modal = document.getElementById("contactModal");
-    let span = document.getElementsByClassName("close")[0];
-    form.addEventListener('submit', function (event) {
-        event.preventDefault();
-        emailjs.sendForm("service_2qjf15k", "housebuddy", this)
-            .then(function () {
-                form.reset()
-                modal.style.display = "block";
-                span.onclick = function () {
-                    modal.style.display = "none";
-                }
-                window.onclick = function (event) {
-                    if (event.target == modal) {
-                        modal.style.display = "none";
-                    }
-                }
-            }, function (error) {
-                alert('Message not sent, try again.', error);
-            });
-    });
-
-}
