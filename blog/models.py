6487b45d7ab7b207503df71e9155e31462984c76
Ajn0r <ronjasjogren@hotmail.com>
2022-08-26 08:52:30 +0000

    make comments approved by default and add docstrings

--- blog/models.py
+++ blog/models.py
@@ -20,6 +20,10 @@ PROCESS = (
 
 
 class Blogpost(models.Model):
+    """
+    The blogpost model, this has been greatly inspired
+    by Code Institute 'I think therefore I blog' module
+    """
     title = models.CharField(max_length=80, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     image = CloudinaryField("image", default='placeholder')
@@ -34,23 +38,35 @@ class Blogpost(models.Model):
         ordering = ['-created_on']
 
     def __str__(self):
-        return self.title
+        """
+        Returns the title as a string
+        """
+        return f"{self.title}"
 
     def total_likes(self):
+        """
+        Method for getting the total amount of likes
+        """
         return self.likes.count()
 
 
-# model for comments
 class Comments(models.Model):
+    """
+    Comment model, this has also been inspired by Code Institue
+    'I think therefore I blog' module
+    """
     post = models.ForeignKey(
         Blogpost, on_delete=models.CASCADE, related_name='comments')
     name = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField(max_length=300)
     posted_on = models.DateTimeField(auto_now_add=True)
-    approved = models.BooleanField(default=False)
+    approved = models.BooleanField(default=True)
 
     class Meta:
         ordering = ['posted_on']
 
     def __str__(self):
+        """
+        returns the name and content of the comment as strings
+        """
         return f'Comment {self.content} by {self.name}'
